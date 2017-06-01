b9e7b2527075f0491e80e02762e6ffa2a1ed01cf
sungjun87 <jakdoo87@naver.com>
2017-06-02 01:15:21 +0900

    로그인 기능

--- src/main/java/lancer/total/persistence/c_loginDAOImpl.java
+++ src/main/java/lancer/total/persistence/c_loginDAOImpl.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import lancer.c_login.domain.c_loginVO;
+import lancer.c_login.domain.c_login_enterpriseVO;
 import lancer.c_login.domain.c_login_freelancerVO;
 @Repository
 public class c_loginDAOImpl implements c_loginDAO {
@@ -18,6 +19,11 @@ public class c_loginDAOImpl implements c_loginDAO {
 	public c_login_freelancerVO select_f_login(c_loginVO vo) {
 		
 		return session.selectOne(namespace+".select_f_login",vo);
+	}
+	@Override
+	public c_login_enterpriseVO select_e_login(c_loginVO vo) {
+		
+		return session.selectOne(namespace+".select_e_login",vo);
 	}
 	
 	
