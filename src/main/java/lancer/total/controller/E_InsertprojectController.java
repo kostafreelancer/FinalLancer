2dce68fa71e020d05638cb82ad03b8e6b096b6c9
home <home@home-PC>
2017-06-02 09:51:22 +0900

    저이제무서워요

---
+++ src/main/java/lancer/total/controller/E_InsertprojectController.java
@@ -0,0 +1,17 @@
+package lancer.total.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/e_insertproject/*")
+public class E_InsertprojectController {
+	
+	
+	@RequestMapping(value = "/e_insertproject", method = RequestMethod.GET)
+	public void e_insertproject(){
+		
+	}
+
+}
