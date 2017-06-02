480206249d03f5b7374707c96cad081396a00824
home <home@home-PC>
2017-06-02 12:21:04 +0900

    ddd

--- src/main/java/lancer/total/persistence/E_InsertprojectDAOImpl.java
+++ src/main/java/lancer/total/persistence/E_InsertprojectDAOImpl.java
@@ -13,22 +13,21 @@ import lancer.e_insertproject.domain.E_Insert;
 @Repository
 public class E_InsertprojectDAOImpl implements E_InsertprojectDAO {
 
-		/*@Inject
-		private SqlSession session;*/
+		@Inject
+		private SqlSession session;
 			
 	    String namespace = "lancer.mappers.e_insertprojectMapper";
 		
 		public void  insertProject(E_Insert project)throws Exception{
-			//session.insert(namespace+".insertProject",project);
+			session.insert(namespace+".insertProject",project);
 		}
 			
 		public int getnum()throws Exception{
-			return 0; 
-			//session.selectOne(namespace+".getnum");
+			return session.selectOne(namespace+".getnum");
 		}
 		
 		public void insertP_Job(Map<String, Integer> map)throws Exception{
-			//session.insert(namespace+".insertP_Job",map);
+			session.insert(namespace+".insertP_Job",map);
 		}
 			
 
